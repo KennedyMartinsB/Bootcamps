ae729da6fe2342ba0aae2e1435d595ec076e7fc6
KennedyMartinsB <Kennedy.Martins@bandtec>
2022-09-12 21:05:55 -0300

    Feature: adição e configuração do ts no node server

---
+++ NLW/esports/server/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+// Primeiro parametro do get pega o que quero que seja listado
+app.get('/ads', (request, response) => {
+    // Como na estrutura de projeto o back end trará um json como response
+    // Devemos trocar o .send por .json e retornar um array
+    return response.json([
+        {id:1, name:'anuncio 1'},
+        {id:2, name:'anuncio 2'},
+        {id:3, name:'anuncio 3'},
+        {id:4, name:'anuncio 4'},
+        {id:5, name:'anuncio 5'}
+    ])
+})
+
+// Listen faz com que a aplicação fique recebendo novas requisições
+// E só pare quando o usuário pedir 
+app.listen(3333)
